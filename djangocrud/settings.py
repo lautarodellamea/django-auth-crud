eafb96ec30629bef75876f6eb997def68e49040e
Lautaro Della Mea <lautarodm98@gmail.com>
2023-06-18 20:07:56 -0300

    databases reconfigured

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "djangocrud.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
         "default": dj_database_url.config(
             default="postgresql://postgres:postgresql@localhost/postgres",
             conn_max_age=600,
